f617a0b3126dab4e00ce8f56f56d8080b2fa3c0f
santanu <santanu.sahoo14112006@gmail.com>
2026-08-15 01:48:06 +0530

    Add LeetCode 238 solution

---
+++ Array/238.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // Answer array
+        vector<int> answer(n, 1);
+
+    
+
+        int left = 1;
+
+        for (int i = 0; i < n; i++) {
+
+        
+            answer[i] = left;
+
+           
+            left = left * nums[i];
+        }
+
+
+
+        int right = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+           
+            answer[i] = answer[i] * right;
+
+           
+            right = right * nums[i];
+        }
+
+        return answer;
+    }
+};
+
+int main() {
+
+    Solution s;
+
+    vector<int> nums = {1, 2, 3, 4};
+
+    vector<int> result = s.productExceptSelf(nums);
+
+   
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
