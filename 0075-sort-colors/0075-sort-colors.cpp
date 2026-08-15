5da62e4860752861cb2a9020a7aa130fa932e429
SANTANU SAHOO <santanu.sahoo14112006@gmail.com>
2026-08-15 20:15:31 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         int count0 = 0;
+        int count0 = 0;
         int count1 = 0;
         int count2 = 0;
 
-        // Count 0, 1 and 2
+        
         for (int x : nums) {
 
             if (x == 0)
@@ -18,19 +18,19 @@ public:
                 count2++;
         }
 
-        // Fill 0
+        
         int i = 0;
 
         while (count0--) {
             nums[i++] = 0;
         }
 
-        // Fill 1
+    
         while (count1--) {
             nums[i++] = 1;
         }
 
-        // Fill 2
+       
         while (count2--) {
             nums[i++] = 2;
         }
