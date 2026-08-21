ac4a926f308941926ef74926d994c11bd2749178
SANTANU SAHOO <santanu.sahoo14112006@gmail.com>
2026-08-22 00:33:31 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.57%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0,end=n-1;
+        while(start<=end){
+           int mid=(start+end)/2;
+
+            if(nums[mid]==target){
+                return mid;
+                break;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+
+
+        
+    }
+};
